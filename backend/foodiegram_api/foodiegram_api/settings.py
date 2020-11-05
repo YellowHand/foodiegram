93ec73d1d43e5f0f081385947e0126f3139c9b48
Paweł Borkowski <ypawelborkowski@gmai.com>
2020-11-05 16:17:41 +0100

    User authentication token

--- backend/foodiegram_api/foodiegram_api/settings.py
+++ backend/foodiegram_api/foodiegram_api/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', # new
 
     # 3rd-party apps
-    'rest_framework',  # new
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',  # new
+    'allauth.account',  # new
+    'allauth.socialaccount',  # new
+    'dj_rest_auth',
+    'dj_rest_auth.registration',  # new
 
     # Local
     'posts', # new
@@ -129,6 +136,13 @@ STATIC_URL = '/static/'
 # new
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [ # new
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # new
+SITE_ID = 1 # new
